509cd8bf5ce0308a9795435197acd663874c69c5
kitakappa <kenkoutenisu@gmail.com>
2016-06-02 11:51:06 +0900

    add license

--- ntlm.js
+++ ntlm.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2013 Sam Decrock https://github.com/SamDecrock/
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+ 
 var crypto = require('crypto');
 
 var flags = {
